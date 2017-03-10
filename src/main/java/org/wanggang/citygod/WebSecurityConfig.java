51f19d5fd60c565be2ce30883ecda56548851b35
Wang Gang <wanggang1987@live.cn>
2017-03-10 22:04:02 +0800

    add webjars import

--- src/main/java/org/wanggang/citygod/WebSecurityConfig.java
+++ src/main/java/org/wanggang/citygod/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
+				.antMatchers("/", "/home","/images/**").permitAll()
 				.anyRequest().authenticated()
 				.and().formLogin().loginPage("/login").permitAll()
 				.and().logout().permitAll();
